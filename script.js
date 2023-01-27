887d5476126b24ea62c6a1c94acdc66fe09460de
Jlinkify <116754620+Jlinkify@users.noreply.github.com>
2023-01-27 16:45:49 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 let interval = null;
 
-document.querySelector("p").onmouseover = event => {  
+document.querySelector("h1").onmouseover = event => {  
   let iteration = 0;
   
   clearInterval(interval);
