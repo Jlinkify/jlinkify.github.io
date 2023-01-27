caf82a388d9ca80889ef1f9c2b2aab20dab6ddae
Jlinkify <116754620+Jlinkify@users.noreply.github.com>
2023-01-27 19:37:48 +0000

    Update script.js

--- TravelAgent/script.js
+++ TravelAgent/script.js
@@ -1,28 +1,22 @@
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let index = 0,
+    interval = 1000;
 
-let interval = null;
+const rand = (min, max) => 
+  Math.floor(Math.random() * (max - min + 1)) + min;
 
-document.querySelector("h1").onmouseover = event => {  
-  let iteration = 0;
-  
-  clearInterval(interval);
-  
-  interval = setInterval(() => {
-    event.target.innerText = event.target.innerText
-      .split("")
-      .map((letter, index) => {
-        if(index < iteration) {
-          return event.target.dataset.value[index];
-        }
-      
-        return letters[Math.floor(Math.random() * 26)]
-      })
-      .join("");
+const animate = star => {
+  star.style.setProperty("--star-left", `${rand(-10, 100)}%`);
+  star.style.setProperty("--star-top", `${rand(-40, 80)}%`);
+
+  star.style.animation = "none";
+  star.offsetHeight;
+  star.style.animation = "";
+}
+
+for(const star of document.getElementsByClassName("magic-star")) {
+  setTimeout(() => {
+    animate(star);
     
-    if(iteration >= event.target.dataset.value.length){ 
-      clearInterval(interval);
-    }
-    
-    iteration += 1 / 3;
-  }, 30);
+    setInterval(() => animate(star), 1000);
+  }, index++ * (interval / 3))
 }
