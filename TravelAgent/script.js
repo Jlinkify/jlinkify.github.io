d25e52edaa8885a53653e28a9c8cc474689309e6
Jlinkify <116754620+Jlinkify@users.noreply.github.com>
2023-01-28 14:20:47 +0000

    Update script.js

--- TravelAgent/script.js
+++ TravelAgent/script.js
@@ -20,3 +20,18 @@ for(const star of document.getElementsByClassName("magic-star")) {
     setInterval(() => animate(star), 1000);
   }, index++ * (interval / 3))
 }
+
+const wrapper = document.getElementById("bubble-wrapper");
+
+const animateBubble = x => {  
+  const bubble = document.createElement("div");
+  
+  bubble.className = "bubble";
+  bubble.style.left = `${x}px`;
+  
+  wrapper.appendChild(bubble);
+  
+  setTimeout(() => wrapper.removeChild(bubble), 2000);
+}
+
+window.onmousemove = e => animateBubble(e.clientX);
