98b96168f1660b0072438e4668d8a48e8f708c36
Jlinkify <116754620+Jlinkify@users.noreply.github.com>
2023-01-28 14:19:47 +0000

    Update script.js

--- Mlinkify/script.js
+++ Mlinkify/script.js
@@ -7,3 +7,18 @@ const handleMove = e => {
 document.onmousemove = e => handleMove(e);
 
 document.ontouchmove = e => handleMove(e.touches[0]);
+
+const wrapper = document.getElementById("bubble-wrapper");
+
+const animateBubble = x => {  
+  const bubble = document.createElement("div");
+  
+  bubble.className = "bubble";
+  bubble.style.left = `${x}px`;
+  
+  wrapper.appendChild(bubble);
+  
+  setTimeout(() => wrapper.removeChild(bubble), 2000);
+}
+
+window.onmousemove = e => animateBubble(e.clientX);
