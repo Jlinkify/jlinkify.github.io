9622945fde34e33c967f0b72666c1ffddb4a0b99
Jlinkify <116754620+Jlinkify@users.noreply.github.com>
2023-02-16 15:59:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+        const cursor = document.querySelector('.cursor');
+
+        document.addEventListener('mousemove', e => {
+            cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+        })
+
+        document.addEventListener('click', () => {
+            cursor.classList.add("expand");
+
+            setTimeout(() => {
+                cursor.classList.remove("expand");
+            }, 500)
+        })
+
+
 document.getElementById("cards").onmousemove = e => {
   for(const card of document.getElementsByClassName("card")) {
     const rect = card.getBoundingClientRect(),
